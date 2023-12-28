6719dfa2d25ad0c16fd85c23c85da352d0dd895f
Yarov Andrey <yarovs.andrey@gmail.com>
2023-12-28 13:56:34 +0200

    инициализация репозитория и создание консольного приложения

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿void WordsUnderThreeSymbols(string[] words)
+{
+    string[] strings = new string[2];
+    int j = 0;
+    for (int i = 0; i < words.Length; i++)
+    {
+        if (words[i].Length <= 3)
+        {
+            strings[j] = words[i];
+            Console.Write(strings[j] + "    ");
+            j++;          
+        }
+    }
+}
+
+string [] words = {"Hello", "2", "world", ":-)" }; 
+
+WordsUnderThreeSymbols(words);
